0473449ac13dd6e5f57742b67472040800f93966
Aaron L. Zeng <a2z@mit.edu>
2016-07-05 04:21:33 +0800

    Fix whitespace

--- init.el
+++ init.el
@@ -261,9 +261,7 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/misc.org"))))
+ '(org-agenda-files (quote ("~/misc.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
