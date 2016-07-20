f834daed147cd697c8d42c01d65f6e7b9c2583a5
Aaron L. Zeng <a2z@mit.edu>
2016-07-19 22:51:29 -0400

    .

--- init.el
+++ init.el
@@ -39,6 +39,7 @@ values."
      python
      ibuffer
      syntax-checking
+     rust
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
