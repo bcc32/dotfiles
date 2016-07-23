98370831b6e6abbfcfa12cd13dc1b45d29d6f349
Aaron L. Zeng <a2z@mit.edu>
2016-07-23 19:33:31 -0400

    Adjust OCaml comment style
    Change the following:
    
    (* let () = *)
    (*   ... *)
    
    to:
    
    (* let () =
     *   ... *)

--- init.el
+++ init.el
@@ -267,6 +267,7 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(comment-style (quote multi-line))
  '(org-agenda-files (quote ("~/misc.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
