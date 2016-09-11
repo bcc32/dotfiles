7fb5c5011741ba804ede6b0caf712b6a4b813cb6
Aaron L. Zeng <a2z@mit.edu>
2016-09-11 02:59:39 -0400

    Add javascript layer

--- init.el
+++ init.el
@@ -39,6 +39,7 @@ values."
      python
      ibuffer
      rust
+     javascript
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
