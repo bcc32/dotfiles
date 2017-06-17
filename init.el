72967bf2872f470e304800c05cbe9347f5ac3412
Aaron L. Zeng <a2z@mit.edu>
2017-06-16 21:56:58 -0400

    Formatting

--- init.el
+++ init.el
@@ -153,7 +153,12 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(zenburn leuven solarized-light solarized-dark)
+   dotspacemacs-themes '(
+                         zenburn
+                         leuven
+                         solarized-light
+                         solarized-dark
+                         )
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
@@ -355,6 +360,16 @@ you should place your code here."
           "~/Google Drive/Internships/Jane Street/2017/internship.org"
           "~/Google Drive/misc.org"
           ))
-  (setq org-export-backends '(ascii gfm html icalendar latex md org reveal))
+  (setq org-export-backends
+        '(
+          ascii
+          gfm
+          html
+          icalendar
+          latex
+          md
+          org
+          reveal
+          ))
   (setq powerline-default-separator 'arrow)
   )
