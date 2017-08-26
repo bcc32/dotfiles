972e97f263f2ebf2b443636f26da6c1cdb51f803
Aaron L. Zeng <a2z@mit.edu>
2017-08-26 12:04:54 +0200

    Add blank custom file

--- init.el
+++ init.el
@@ -330,7 +330,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-  (setq custom-file "/dev/null")
+  (setq custom-file "~/.spacemacs.d/custom.el")
   )
 
 (defun dotspacemacs/user-config ()
