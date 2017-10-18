f80c349e7caa0cf12f1f75e0f8c9b1f5f751b61b
Aaron L. Zeng <a2z@mit.edu>
2017-10-18 11:51:37 -0400

    Add yaml layer

--- init.el
+++ init.el
@@ -81,6 +81,7 @@ values."
      latex
      markdown
      org
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
