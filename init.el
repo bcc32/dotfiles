62e45cfd76bf02256902676e1dd76dddebea48c4
Aaron L. Zeng <me@bcc32.com>
2017-11-09 02:08:28 -0500

    Fix for ivy-use-virtual-buffers

--- init.el
+++ init.el
@@ -375,10 +375,12 @@ you should place your code here."
     'merlin-enclosing-expand)
   ;; https://github.com/syl20bnr/spacemacs/issues/480
   (add-hook 'cperl-mode-hook (lambda () (local-unset-key (kbd "{"))))
+  ;; https://stackoverflow.com/questions/44796844/switch-off-ivy-use-virtual-buffers-in-spacemacs
+  (with-eval-after-load 'recentf
+    (setq ivy-use-virtual-buffers nil))
   (setq comment-style 'multi-line)
   (setq company-idle-delay 1.0)
   (setq fill-column 80)
-  (setq ivy-use-virtual-buffers nil)
   (setq js-indent-level 2)
   (setq powerline-default-separator 'arrow)
   (load "~/.spacemacs.d/PG/generic/proof-site")
