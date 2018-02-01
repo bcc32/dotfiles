f54ef0c9be776fe9df5be9639add8d43137c468e
Aaron L. Zeng <me@bcc32.com>
2018-02-01 17:41:29 -0500

    Fix OBOE in `reconcile` report

--- init.el
+++ init.el
@@ -383,6 +383,6 @@ you should place your code here."
   (setq js-indent-level 2)
   (setq powerline-default-separator 'arrow)
   (with-eval-after-load 'ledger-mode
-    (push '("reconcile" "%(binary) -f %(ledger-file) r --effective --sort d -d \"d>[this month]\" -C %(account)")
+    (push '("reconcile" "%(binary) -f %(ledger-file) r --effective --sort d -d \"d>=[this month]\" -C %(account)")
           ledger-reports))
   )
