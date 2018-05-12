f9adfc070b21430a7d297c630b4101307dcee133
Aaron L. Zeng <me@bcc32.com>
2018-05-12 00:39:03 -0400

    Merge some of JS xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,7 +1,48 @@
-import XMonad
-import XMonad.Hooks.DynamicLog (xmobar)
+-- Warnings generated from compiling this module will be written to
+-- ~/.xmonad/xmonad.errors.
+{-# OPTIONS -Wall #-}
 
-main = xmonad =<< xmobar def
-  { modMask = mod1Mask
-  , terminal = "urxvtc"
-  }
+import XMonad
+import XMonad.Hooks.DynamicLog    (xmobar)
+import XMonad.Hooks.EwmhDesktops  (ewmh)
+import XMonad.Layout.GridVariants (SplitGrid (..), Orientation (..))
+import XMonad.Layout.Named        (named)
+import XMonad.Layout.NoBorders    (smartBorders)
+import XMonad.Layout.Tabbed       (tabbed, shrinkText)
+import XMonad.Layout.WorkspaceDir (changeDir, workspaceDir)
+import XMonad.Prompt.Shell        (shellPrompt)
+import XMonad.Util.EZConfig       (additionalKeysP)
+
+myBindings :: [(String, X ())]
+myBindings =
+  [ ("M-d", changeDir def)
+  , ("M-p", shellPrompt def)
+  , ("M-x", spawn "gnome-screensaver-command -l")
+  , ("M-S-p e", spawn "emacs")
+  , ("M-S-p c", spawn "google-chrome")
+  , ("M-S-p f", spawn "firefox")
+  ]
+
+main :: IO ()
+main = do
+  spawn "gnome-screensaver"
+  cfg <- xmobar def
+    { modMask           = mod1Mask
+    , terminal          = "urxvtc"
+    , layoutHook        = (workspaceDir "~" . smartBorders) myLayoutHook
+    , focusFollowsMouse = False
+    }
+  xmonad $ ewmh $ cfg `additionalKeysP` myBindings
+  where
+    myLayoutHook
+      =   (named "Split Grid T" . splitGrid) T
+      ||| (named "Split Grid L" . splitGrid) L
+      ||| (named "Tabbed" . tabbed shrinkText) def
+      where
+        masterRows  = 1
+        slaveRows   = 1
+        masterRatio = 5/8
+        aspect      = realToFrac $ (1 + sqrt 5 :: Double) / 2
+        increment   = 1/20
+        splitGrid orientation =
+          SplitGrid orientation masterRows slaveRows masterRatio aspect increment
