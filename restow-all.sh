59d253dc706628a0bf653ad64c3a7484395a34a9
Aaron L. Zeng <me@bcc32.com>
2018-05-20 04:52:50 -0400

    Pre-create ~/.ssh

--- restow-all.sh
+++ restow-all.sh
@@ -5,7 +5,7 @@ set -eu
 # Ensure certain directories already exist so that stow won't try to create
 # symlinks too high in the file hierarchy, leading to unmanaged files appearing
 # in the repo.
-mkdir -p ~/.config ~/.stack
+mkdir -p ~/.config ~/.ssh ~/.stack
 
 FLAGS=${FLAGS--R}
 DIRS=*/
