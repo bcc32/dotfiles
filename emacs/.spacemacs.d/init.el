0d2014ac0463c6af2946bda05f305fd02cd83aa7
Aaron L. Zeng <me@bcc32.com>
2018-09-17 10:37:53 -0400

    Make sure to load custom-file

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -523,7 +523,8 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
-  (setq custom-file "~/.spacemacs.d/custom.el"))
+  (setq custom-file "~/.spacemacs.d/custom.el")
+  (load custom-file))
 
 (defun dotspacemacs/user-load ()
   "Library to load while dumping.
