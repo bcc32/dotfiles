2fa6738de77215119493d6441b56886ce4565baa
Aaron L. Zeng <me@bcc32.com>
2018-10-03 14:24:09 -0400

    Revert "Enable version-control layer"
    This reverts commit 350ad20a4c0ad3b6a92bc84f6e2dad37e0c31512.

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -95,7 +95,6 @@ This function should only modify configuration layer settings."
      ;; Programmer tools
      git
      (shell :variables shell-default-shell 'multi-term)
-     version-control
      ;; Programming languages
      ;; Functional
      emacs-lisp
