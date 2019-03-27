d5b5f38eb321ef749a849d87b3280b5865979454
Aaron L. Zeng <me@bcc32.com>
2019-03-27 19:02:37 -0400

    Allow bcc32/ocamlformat-on-save-mode in .dir-locals.el

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(midnight-mode t))
+ '(midnight-mode t)
+ '(safe-local-variable-values
+   (quote
+    ((bcc32/ocamlformat-on-save-mode . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
