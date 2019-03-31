10e1dadb3005e3a01b89c4aa389d68b9ae337455
Aaron L. Zeng <me@bcc32.com>
2019-03-31 18:13:16 -0400

    Shellcheck pick.sh

--- scripts/bin/pick.sh
+++ scripts/bin/pick.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -euo pipefail
 
@@ -6,4 +6,4 @@ N=${N:-20}
 
 mkdir temp
 
-find -maxdepth 1 -type f -print0 | shuf -zn "$N" | xargs -0 mv -t ./temp
+find . -maxdepth 1 -type f -print0 | shuf -zn "$N" | xargs -0 mv -t ./temp
