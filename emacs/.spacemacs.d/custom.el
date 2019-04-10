571ab527c27b5a8c041069ce6dabc073e24d6d3e
Aaron L. Zeng <me@bcc32.com>
2019-04-10 17:19:04 -0400

    Configure some spaceline toggles
    - Enable highlighting the file name in the buffer path
    - Generally shorten display

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(midnight-mode t)
- '(safe-local-variable-values (quote ((bcc32/ocamlformat-on-save-mode . t)))))
+ '(safe-local-variable-values (quote ((bcc32/ocamlformat-on-save-mode . t))))
+ '(spaceline-all-the-icons-flycheck-alternate t)
+ '(spaceline-all-the-icons-hide-long-buffer-path t)
+ '(spaceline-all-the-icons-highlight-file-name t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
