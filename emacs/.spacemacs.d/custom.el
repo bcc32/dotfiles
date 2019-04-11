b2b15cf4f4eabae02a98560f2fcabd52b0b823ec
Aaron L. Zeng <me@bcc32.com>
2019-04-11 16:04:51 -0400

    Configure some more Spaceline variables

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -9,7 +9,12 @@
  '(sentence-end-double-space t)
  '(spaceline-all-the-icons-flycheck-alternate t)
  '(spaceline-all-the-icons-hide-long-buffer-path t)
- '(spaceline-all-the-icons-highlight-file-name t))
+ '(spaceline-all-the-icons-highlight-file-name t)
+ '(spaceline-all-the-icons-icon-set-eyebrowse-slot (quote square))
+ '(spaceline-all-the-icons-icon-set-window-numbering (quote string))
+ '(spaceline-all-the-icons-separator-type (quote arrow))
+ '(spaceline-all-the-icons-slim-render nil)
+ '(spaceline-all-the-icons-window-number-always-visible t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
