29fa4822e5c9934f647d014f3d61da1d574ec51a
Aaron L. Zeng <me@bcc32.com>
2019-06-20 18:20:45 -0400

    Switch back to Iosevka as Emacs font

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -476,7 +476,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Fantasque Sans Mono"
+   dotspacemacs-default-font '("Iosevka"
                                :size 14
                                :weight normal
                                :width normal)
