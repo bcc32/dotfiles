0d777189e9ca18ec1303228deeb3d210faade99b
Aaron L. Zeng <me@bcc32.com>
2019-10-21 22:45:32 -0400

    Workaround for font size issue on MacOS
    Floating-point font size is supposed to be point size rather than
    pixel size, but it doesn't seem to work on MacOS.

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -513,8 +513,8 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
 
    ;; Default font or prioritized list of fonts.
-   dotspacemacs-default-font '("Iosevka"
-                               :size 10.5
+   dotspacemacs-default-font `("Iosevka"
+                               :size ,(if (eq system-type 'darwin) 14 10.5)
                                :weight normal
                                :width normal)
 
