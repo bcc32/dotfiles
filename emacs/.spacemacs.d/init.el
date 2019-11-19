805593775be646bed8bd2c75c73df2a23b7ec13b
Aaron L. Zeng <me@bcc32.com>
2019-11-19 16:51:02 -0500

    Add ess layer

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -261,6 +261,8 @@ This function should only modify configuration layer settings."
      ,@(when-any-installed '("python") 'python)
      ,@(when-any-installed '("ruby") 'ruby)
      shell-scripts
+     ;; Statistical
+     ,@(when-any-installed '("R") 'ess)
      ;; Documents/markup
      csv
      json
