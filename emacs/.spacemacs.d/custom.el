4da4c2b012f082d6f3b5faa4e6cc52005da5d256
Aaron L. Zeng <me@bcc32.com>
2020-08-31 18:11:09 -0400

    Update custom.el format to Emacs 27.1

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-engine (quote xetex))
- '(browse-url-browser-function (quote bcc32/browse-url-on-ssh-client-if-exists))
+ '(TeX-engine 'xetex)
+ '(browse-url-browser-function 'bcc32/browse-url-on-ssh-client-if-exists)
  '(calc-context-sensitive-enter t)
  '(calc-settings-file "~/.spacemacs.d/calc-settings.el")
  '(counsel-find-file-at-point nil)
@@ -12,79 +12,67 @@
  '(cperl-indent-parens-as-block t t)
  '(dired-async-mode t)
  '(enable-recursive-minibuffers t)
- '(epa-pinentry-mode (quote loopback))
+ '(epg-pinentry-mode 'loopback)
  '(eshell-visual-commands
-   (quote
-    ("ncdu" "vi" "screen" "top" "less" "more" "lynx" "ncftp" "pine" "tin" "trn" "elm")))
- '(eshell-visual-subcommands (quote (("git" "log" "diff" "show"))))
+   '("ncdu" "vi" "screen" "top" "less" "more" "lynx" "ncftp" "pine" "tin" "trn" "elm"))
+ '(eshell-visual-subcommands '(("git" "log" "diff" "show")))
  '(evil-want-Y-yank-to-eol t)
  '(flycheck-idle-change-delay 1.0)
- '(geiser-default-implementation (quote chicken))
+ '(geiser-default-implementation 'chicken)
  '(gofmt-command "goimports")
  '(grep-find-ignored-directories
-   (quote
-    ("_build" "SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}")))
+   '("_build" "SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}"))
  '(helm-make-nproc 0)
  '(ivy-use-virtual-buffers nil)
  '(ledger-accounts-file (expand-file-name "declarations.ldg" "~/journal"))
  '(ledger-reports
-   (quote
-    (("reconcile" "ldg-reconcile %(account)")
+   '(("reconcile" "ldg-reconcile %(account)")
      ("uncleared" "%(binary) reg -U '^Assets' '^Equity' '^Liabilities'")
      ("bal" "%(binary) bal")
      ("payee" "%(binary) reg @%(payee)")
      ("account" "%(binary) reg %(account)")
-     ("validate" "%(binary) source %(env-ledger-file)"))))
+     ("validate" "%(binary) source %(env-ledger-file)")))
  '(midnight-mode t)
  '(minibuffer-depth-indicate-mode t)
  '(mode-line-bell-mode t)
  '(org-agenda-dim-blocked-tasks t)
  '(org-agenda-files (expand-file-name "agenda-files" org-directory))
  '(org-agenda-skip-scheduled-if-done t)
- '(org-agenda-span (quote day))
+ '(org-agenda-span 'day)
  '(org-agenda-tags-todo-honor-ignore-options t)
- '(org-agenda-todo-ignore-scheduled (quote future))
+ '(org-agenda-todo-ignore-scheduled 'future)
  '(org-agenda-todo-ignore-time-comparison-use-seconds t)
  '(org-attach-commit nil t nil "This variable is obsolete but has effect in some older versions of org-mode.")
- '(org-capture-templates (quote (("t" "Todo" entry (file "") "* TODO %?
+ '(org-capture-templates '(("t" "Todo" entry (file "") "* TODO %?
 %U
-%a"))))
+%a")))
  '(org-clock-persist t)
  '(org-default-notes-file (expand-file-name "refile.org" org-directory))
  '(org-default-priority 67)
  '(org-enforce-todo-checkbox-dependencies t)
  '(org-enforce-todo-dependencies t)
- '(org-export-backends (quote (ascii html icalendar latex md org texinfo)))
+ '(org-export-backends '(ascii html icalendar latex md org texinfo))
  '(org-habit-show-habits-only-for-today nil)
- '(org-html-htmlize-output-type (quote css))
+ '(org-html-htmlize-output-type 'css)
  '(org-insert-heading-respect-content t)
  '(org-log-into-drawer t)
- '(org-log-redeadline (quote note))
- '(org-log-reschedule (quote time))
+ '(org-log-redeadline 'note)
+ '(org-log-reschedule 'time)
  '(org-modules
-   (quote
-    (ol-bibtex ol-docview org-habit ol-info org-tempo ol-eshell org-checklist ol-man)))
+   '(ol-bibtex ol-docview org-habit ol-info org-tempo ol-eshell org-checklist ol-man))
  '(org-outline-path-complete-in-steps nil)
- '(org-refile-targets
-   (quote
-    ((nil :maxlevel . 10)
-     (org-agenda-files :maxlevel . 10))))
- '(org-refile-use-outline-path (quote buffer-name))
+ '(org-refile-targets '((nil :maxlevel . 10) (org-agenda-files :maxlevel . 10)))
+ '(org-refile-use-outline-path 'buffer-name)
  '(org-startup-indented t)
  '(org-startup-shrink-all-tables t)
- '(org-stuck-projects
-   (quote
-    ("+LEVEL<=2/!"
-     ("TODO" "MAYBE" "INPROGRESS")
-     nil "")))
+ '(org-stuck-projects '("+LEVEL<=2/!" ("TODO" "MAYBE" "INPROGRESS") nil ""))
  '(org-todo-keywords
-   (quote
-    ((sequence "TODO(t)" "MAYBE(m/!)" "INPROGRESS(p!)" "STALLED(a)" "BLOCKED(k@/!)" "INREVIEW(r!)" "|" "DONE(d/@)" "DELEGATED(g@)" "DEFERRED(e!)" "NOTDONE(n)")
+   '((sequence "TODO(t)" "MAYBE(m/!)" "INPROGRESS(p!)" "STALLED(a)" "BLOCKED(k@/!)" "INREVIEW(r!)" "|" "DONE(d/@)" "DELEGATED(g@)" "DEFERRED(e!)" "NOTDONE(n)")
      (type "BUG(b/!)" "CLEANUP(l/!)" "|" "FIXED(x/@)" "WONTFIX(w@/@)")
-     (type "ENHANCEMENT(h/!)" "FEATURE(f/!)" "|" "RELEASED(s/@)" "CANCELED(c/@)"))))
- '(projectile-ignored-project-function (quote bcc32/projectile-ignored-project-function))
+     (type "ENHANCEMENT(h/!)" "FEATURE(f/!)" "|" "RELEASED(s/@)" "CANCELED(c/@)")))
+ '(projectile-ignored-project-function 'bcc32/projectile-ignored-project-function)
  '(rust-format-on-save t)
- '(safe-local-variable-values (quote ((bcc32/ocamlformat-on-save-mode . t))))
+ '(safe-local-variable-values '((bcc32/ocamlformat-on-save-mode . t)))
  '(sentence-end-double-space t)
  '(solarized-distinct-doc-face t)
  '(solarized-use-more-italic t)
