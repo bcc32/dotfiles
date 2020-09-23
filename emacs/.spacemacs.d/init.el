f4f5d1f2bb0429c0900fccc8ccb0483c2bb5277e
Aaron L. Zeng <me@bcc32.com>
2020-09-23 19:56:07 -0400

    Add FIXME

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -716,6 +716,8 @@ before packages are loaded."
 
   ;; Superword mode, for evil
   (with-eval-after-load 'evil
+    ;; FIXME: Should also change the syntax table in tuareg because it affects
+    ;; (describe-function 'capitalize), which affects [qmli] abbrev.
     (defalias 'forward-evil-word #'forward-evil-symbol)
     ;; make evil-search-word look for symbol rather than word boundaries
     (setq-default evil-symbol-word-search t))
