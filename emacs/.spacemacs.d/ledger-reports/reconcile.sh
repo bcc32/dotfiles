48d21b0aca53e91872830a4fe3220d4618b81642
Aaron L. Zeng <me@bcc32.com>
2021-02-27 21:59:07 -0500

    ledger-mode: Fix accidental pipeline in report script

--- emacs/.spacemacs.d/ledger-reports/reconcile.sh
+++ emacs/.spacemacs.d/ledger-reports/reconcile.sh
@@ -1,12 +1,19 @@
+COLUMNS="$(BINARY eval columns)"
+
+print_hline() {
+    printf %"$COLUMNS"s | tr ' ' '-'
+    printf "\n"
+}
+
 query='/^%(account)$/'
-echo -----------------
-echo | Cleared       |
-echo -----------------
+
+print_hline
+echo Cleared
+print_hline
 BINARY reg --cleared --current --effective --sort d -d "d>=[90 days ago]" --tail 20 "$query" "$@"
 echo
 
-printf %"$COLUMNS"s | tr " " "-"
-echo -----------------
-echo | Uncleared     |
-echo -----------------
+print_hline
+echo Uncleared
+print_hline
 BINARY reg --uncleared --current --sort d "$query" "$@"
