36217d359ccb13b19d87f3d7c63dd3bbec334f6b
Aaron L. Zeng <me@bcc32.com>
2021-03-04 23:26:44 -0500

    Only use spacemacs env file on macOS

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -669,7 +669,8 @@ This function defines the environment variables for your Emacs session. By
 default it calls `spacemacs/load-spacemacs-env' which loads the environment
 variables declared in `~/.spacemacs.env' or `~/.spacemacs.d/.spacemacs.env'.
 See the header of this file for more information."
-  (spacemacs/load-spacemacs-env))
+  (when (eq system-type 'darwin)
+    (spacemacs/load-spacemacs-env)))
 
 (defun dotspacemacs/user-init ()
   "Initialization for user code:
