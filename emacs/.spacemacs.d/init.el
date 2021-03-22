d78fca739e372f1ec8194ffcfc7c115591f7a989
Aaron L. Zeng <me@bcc32.com>
2021-03-21 20:10:04 -0400

    Remove redundant try-smerge-hook
    vc-git actually does this already in `vc-git-find-file-hook`.

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -71,16 +71,6 @@ unset in the selected frame, passing ARGS."
   "Set `fill-column' to 70 characters in derived modes of `text-mode'."
   (setq-local fill-column 70))
 
-(defun bcc32//try-smerge-hook ()
-  "Enable `smerge-mode' if there are conflict markers in the buffer.
-
-This function is intended to be used with some hook like `find-file-hook' or
-`after-revert-hook'."
-  (save-excursion
-    (goto-char (point-min))
-    (when (re-search-forward "^<<<<<<< " (* 1000 1000) t)
-      (smerge-mode +1))))
-
 (defun dotspacemacs/layers ()
   "Layer configuration:
 This function should only modify configuration layer settings."
@@ -740,9 +730,6 @@ before packages are loaded."
   (add-hook 'text-mode-hook #'turn-on-auto-fill)
 
   (add-hook 'dired-mode-hook #'dired-omit-mode)
-
-  (add-hook 'after-revert-hook #'bcc32//try-smerge-hook)
-  (add-hook 'find-file-hook #'bcc32//try-smerge-hook)
 
   ;; Make sure my customizations take precedence over settings that Spacemacs
   ;; `setq's, even after running `dotspacemacs/sync-configuration-layers'.
