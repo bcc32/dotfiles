9ccbd94ab977abcdecd30dccf27bbd9d74527b52
Aaron L. Zeng <me@bcc32.com>
2021-05-10 16:15:15 -0400

    Remove customization for org-extend-today-until
    This option seems to break the default input when rescheduling an
    existing task.  When the existing timestamp does not have a time of
    day, the starting date is unexpectedly set to the day before the
    existing timestamp.

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -90,7 +90,6 @@
  '(org-enforce-todo-checkbox-dependencies t)
  '(org-enforce-todo-dependencies t)
  '(org-export-backends '(ascii gfm html icalendar latex md org texinfo))
- '(org-extend-today-until 2)
  '(org-habit-show-habits-only-for-today nil)
  '(org-html-htmlize-output-type 'css)
  '(org-insert-heading-respect-content t)
