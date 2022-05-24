00d7d34cf5a8054af58737beb8259aa4a9ca1328
Aaron L. Zeng <me@bcc32.com>
2022-05-23 23:03:43 -0400

    Un-disable M-x list-timers

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -818,6 +818,8 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
 
+  (put 'list-timers 'disabled nil)
+
   (bind-key "C-x C-c" (lambda () (interactive) (error "Do not use C-x C-c")))
 
   ;; Superword mode, for evil
