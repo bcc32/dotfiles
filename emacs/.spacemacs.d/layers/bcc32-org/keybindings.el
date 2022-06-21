fe97a70e0cc60555aa1412ac5c0b75f167deabb4
Aaron L. Zeng <me@bcc32.com>
2022-06-21 18:57:27 -0400

    Remove bcc32-org-sort* key bindings

--- emacs/.spacemacs.d/layers/bcc32-org/keybindings.el
+++ emacs/.spacemacs.d/layers/bcc32-org/keybindings.el
@@ -1,7 +1,4 @@
 (when (configuration-layer/package-used-p 'org)
   (spacemacs/set-leader-keys
     "aob" 'org-switchb
-    "aog" 'counsel-org-goto-all)
-  (spacemacs/set-leader-keys-for-major-mode 'org-mode
-    "St" 'bcc32-org-sort-by-closed
-    "Sa" 'bcc32-org-sort-entire-agenda))
+    "aog" 'counsel-org-goto-all))
