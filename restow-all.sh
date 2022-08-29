f5bdace2dbdcb158f8a6dc8a55119618197b3456
Aaron L. Zeng <me@bcc32.com>
2022-08-29 17:46:48 -0400

    shfmt restow-all.sh

--- restow-all.sh
+++ restow-all.sh
@@ -11,18 +11,18 @@ packages=(*/)
 filtered_packages=()
 
 for package in "${packages[@]}"; do
-    skip=0
+  skip=0
 
-    if [ "$package" = "X/" ]; then
-        case "$(uname -s)" in
-        Darwin*) skip=1 ;;
-        *) ;;
-        esac
-    fi
+  if [ "$package" = "X/" ]; then
+    case "$(uname -s)" in
+    Darwin*) skip=1 ;;
+    *) ;;
+    esac
+  fi
 
-    if ! ((skip)); then
-        filtered_packages+=("$package")
-    fi
+  if ! ((skip)); then
+    filtered_packages+=("$package")
+  fi
 done
 
 ./stow.sh -R "$@" "${filtered_packages[@]}"
