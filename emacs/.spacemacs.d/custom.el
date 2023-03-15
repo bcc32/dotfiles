77384ba3bae174b237cdcb8b646b4674cf4d6d37
Aaron L. Zeng <me@bcc32.com>
2023-03-14 23:49:42 -0400

    Customize org-extend-today-until
    The bug causing me to previously remove the customization was fixed;
    see https://list.orgmode.org/87bkkzy6nc.fsf@localhost/

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -105,6 +105,7 @@
  '(org-enforce-todo-checkbox-dependencies t)
  '(org-enforce-todo-dependencies t)
  '(org-export-backends '(ascii gfm html icalendar latex md org texinfo))
+ '(org-extend-today-until 2)
  '(org-habit-graph-column 80)
  '(org-habit-show-habits-only-for-today nil)
  '(org-html-htmlize-output-type 'css)
