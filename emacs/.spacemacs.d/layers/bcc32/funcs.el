fdd6348780c1712e31cd4d8a8847381ce90256b9
Aaron L. Zeng <me@bcc32.com>
2023-12-29 16:51:23 -0500

    Fix missing docstring for bcc32-hard-disable-command

--- emacs/.spacemacs.d/layers/bcc32/funcs.el
+++ emacs/.spacemacs.d/layers/bcc32/funcs.el
@@ -2,6 +2,9 @@
   "Raise an error and educate the user not to press those keys.
 
 The error message contains whatever keys were used to invoke the
-current command, so you can "
+current command, so you can simply bind a key to this command and
+it will display the right message, e.g.:
+
+\(bind-key \"<f1>\" #\\='bcc32-hard-disable-command)"
   (interactive)
   (user-error "Do not use %s" (key-description (this-command-keys))))
