cfc8fce4899e331560ea914aa49f2a67c19a3664
Aaron L. Zeng <me@bcc32.com>
2024-01-06 21:26:35 -0500

    Display ledger-reconcile balance in header

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -63,6 +63,7 @@
  '(ledger-accounts-file (expand-file-name "declarations.ldg" "~/journal"))
  '(ledger-default-date-format ledger-iso-date-format)
  '(ledger-reconcile-default-commodity "USD")
+ '(ledger-reconcile-display-balance-in-header t)
  '(ledger-reconcile-insert-effective-date 'bcc32-ledger-should-insert-effective-date)
  '(ledger-report-use-header-line t)
  '(ledger-reports
