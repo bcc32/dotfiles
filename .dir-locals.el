1819a8b5f18be2e3b43b5b715a9b59420a415221
Aaron L. Zeng <me@bcc32.com>
2024-01-17 02:15:06 -0500

    Make magit-todos exclude .vendor/ dir

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-mode . ((magit-todos-rg-extra-args . ("--hidden")))))
+((magit-mode . ((magit-todos-exclude-globs . (".git/" ".vendor/"))
+                (magit-todos-rg-extra-args . ("--hidden")))))
