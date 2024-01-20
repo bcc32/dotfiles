0f8dfee8825204bf33d6bfc04896c01a0c2c1e67
Aaron L. Zeng <me@bcc32.com>
2024-01-20 17:48:41 -0500

    Remove "cleared" report
    Use ledger-balance-at-point instead.

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -68,8 +68,7 @@
  '(ledger-reconcile-insert-effective-date 'bcc32-ledger-should-insert-effective-date)
  '(ledger-report-use-header-line t)
  '(ledger-reports
-   '(("cleared" "%(binary) -f %(ledger-file) bal --current -C %(account)")
-     ("debts" "ledger register --by-payee 'Personal Debts'")
+   '(("debts" "ledger register --by-payee 'Personal Debts'")
      ("reconcile" "%(binary) -f %(ledger-file) reg --current --effective --sort '(X ? 0 : 1), d' -d \"!X || d>=[90 days ago]\" \"/^%(account)$/\" | tac")
      ("reimbursement" "%(binary) -f %(ledger-file) --group-by payee reg -U '^Assets:Reimbursable Expenses'")
      ("uncleared" "%(binary) -f %(ledger-file) reg --current -U --group-by account '^Assets' '^Equity' '^Liabilities'")
