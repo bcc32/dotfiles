0eff6837fd7c5350a7ced3f44afbd9de52c03d6d
Aaron L. Zeng <me@bcc32.com>
2024-02-24 01:06:02 -0500

    bcc32-org-lint: Implement as org lint checker instead of flycheck

--- emacs/.spacemacs.d/layers/bcc32-org/local/bcc32-org/bcc32-org-lint.el
+++ emacs/.spacemacs.d/layers/bcc32-org/local/bcc32-org/bcc32-org-lint.el
@@ -12,9 +12,9 @@
 ;;; Code:
 
 (require 'org)
-(require 'flycheck)
+(require 'org-lint)
 
-(defun bcc32-org-flycheck--map-entry-children (proc)
+(defun bcc32-org-lint--map-entry-children (proc)
   "Call PROC with point at each child of the current entry.
 
 Collect the values returned by PROC into a list."
@@ -26,50 +26,43 @@ Collect the values returned by PROC into a list."
           (push (funcall proc) results)))
       (nreverse results))))
 
-(defun bcc32-org-flycheck--entry-has-todo-children-p ()
+(defun bcc32-org-lint--entry-has-todo-children-p ()
   "Return non-nil if the current entry has a child with any TODO state."
-  (cl-some #'identity (bcc32-org-flycheck--map-entry-children #'org-get-todo-state)))
+  (cl-some #'identity (bcc32-org-lint--map-entry-children #'org-get-todo-state)))
 
-(defconst bcc32-org//statistics-cookie-re
+(defconst bcc32-org-lint--statistics-cookie-regexp
   (rx "[" (0+ digit) (or "%" (: "/" (0+ digit))) "]")
   "Pattern that matches `org-mode' statistics cookies.
 
 Copied from `org-element-statistics-cookie-parser'.")
 
-(defun bcc32-org-flycheck--lint-headline-statistics-cookie (checker)
-  "Lint the current headline for statistics cookies using CHECKER."
-  (let ((heading (org-get-heading :no-tags :no-todo)))
-    (when (and (bcc32-org-flycheck--entry-has-todo-children-p)
-               (not (string-match-p bcc32-org//statistics-cookie-re heading)))
-      (flycheck-error-new-at (line-number-at-pos)
-                             nil
-                             'error
-                             "heading has no statistics cookie"
-                             :checker checker))))
+(defun bcc32-org-lint--lint-headline-statistics-cookie ()
+  "Return non-nil if the heading at point is missing a statistics cookie.
 
-(defun bcc32-org-flycheck--lint-buffer (checker)
-  "Lint the current buffer for statistics cookies using CHECKER."
+The return value is a list (POS MESSAGE) containing the current
+position and an error message, suitable for
+`org-lint-add-checker'.
+
+Only returns non-nil if this heading is missing a statistics
+cookie but should have one, i.e., if it has children with TODO
+keywords."
+  (let ((heading (org-get-heading :no-tags :no-todo)))
+    (when (and (bcc32-org-lint--entry-has-todo-children-p)
+               (not (string-match-p bcc32-org-lint--statistics-cookie-regexp heading)))
+      (list (point) "heading has no statistics cookie"))))
+
+;; TODO: Use parse-tree instead of crawling the buffer by hand
+(defun bcc32-org-lint-statistics-cookies (_parse-tree)
+  "Report missing statistics cookies for headings with TODO subitems."
   (delq nil
         (org-map-entries
          (lambda ()
-           (bcc32-org-flycheck--lint-headline-statistics-cookie checker)))))
+           (bcc32-org-lint--lint-headline-statistics-cookie)))))
 
-(defun bcc32-org-flycheck--start (checker callback)
-  "Start linting an org buffer for syntax checker CHECKER.
-
-CALLBACK should be a flycheck checker callback."
-  (condition-case err
-      (let ((errors (bcc32-org-flycheck--lint-buffer checker)))
-        (funcall callback 'finished errors))
-    (error (funcall callback 'errored (error-message-string err)))))
-
-(flycheck-define-generic-checker 'bcc32-org-lint
-  "An org linter to enforce statistics cookies where appropriate."
-  :start #'bcc32-org-flycheck--start
-  :modes 'org-mode
-  :predicate #'org-agenda-file-p)
-
-(add-to-list 'flycheck-checkers 'bcc32-org-lint)
+(org-lint-add-checker 'bcc32-org-statistics-cookies
+  "Report missing statistics cookies for headings with TODO subitems"
+  #'bcc32-org-lint-statistics-cookies
+  :categories '(bcc32-org))
 
 (provide 'bcc32-org-lint)
 ;;; bcc32-org-lint.el ends here
