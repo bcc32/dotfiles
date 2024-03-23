8acf6a083cc24c1b018f69812005cc89256c4188
Aaron L. Zeng <me@bcc32.com>
2024-03-23 15:46:13 -0400

    Make SPC aog key binding work with compleseus layer

--- emacs/.spacemacs.d/layers/bcc32-org/keybindings.el
+++ emacs/.spacemacs.d/layers/bcc32-org/keybindings.el
@@ -1,6 +1,14 @@
 ;; -*- lexical-binding: t; -*-
 
+(defun bcc32-org/goto-agenda-heading ()
+  "Prompt and visit an org agenda heading."
+  (interactive)
+  (call-interactively
+   (or (seq-find #'fboundp '(counsel-org-goto-all
+                             consult-org-agenda))
+       (error "No command available to prompt for org agendas"))))
+
 (when (configuration-layer/package-used-p 'org)
   (bind-keys :map spacemacs-default-map
              ("aob" . org-switchb)
-             ("aog" . counsel-org-goto-all)))
+             ("aog" . bcc32-org/goto-agenda-heading)))
