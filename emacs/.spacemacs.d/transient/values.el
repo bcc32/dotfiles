c349c9c08be600ac1bbf4aacfd48a2ad165eff52
Aaron L. Zeng <me@bcc32.com>
2024-03-24 22:45:32 -0400

    Disable external diff tool when viewing stash diffs

--- emacs/.spacemacs.d/transient/values.el
+++ emacs/.spacemacs.d/transient/values.el
@@ -1,2 +1,3 @@
-((magit-log:magit-status-mode "-n256" "--graph" "--decorate")
+((magit-diff:magit-stash-mode "--no-ext-diff")
+ (magit-log:magit-status-mode "-n256" "--graph" "--decorate")
  (magit-submodule "--recursive"))
