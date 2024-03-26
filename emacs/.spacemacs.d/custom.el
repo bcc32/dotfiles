10a072440a56a60b4494e65d5718ecd57d986f9e
Aaron L. Zeng <me@bcc32.com>
2024-03-26 00:13:34 -0400

    Increase org-web-tools-pandoc-sleep-time
    pandoc takes a bit longer to start for the first time on my MacBook.

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -139,6 +139,7 @@
    '((sequence "TODO(t)" "MAYBE(m/!)" "INPROGRESS(p!)" "STALLED(a)" "BLOCKED(k@/!)" "INREVIEW(r!)" "|" "DONE(d/@)" "DELEGATED(g@)" "DEFERRED(e!)" "NOTDONE(n)")
      (type "BUG(b/!)" "CLEANUP(l/!)" "|" "FIXED(x/@)" "WONTFIX(w@/@)")
      (type "ENHANCEMENT(h/!)" "FEATURE(f/!)" "|" "RELEASED(s/@)" "CANCELED(c/@)")))
+ '(org-web-tools-pandoc-sleep-time 0.5)
  '(org-wild-notifier-alert-time '(10 2))
  '(paradox-github-token t nil nil "Disable GitHub integration in Paradox")
  '(pdf-tools-installer-os "nixos")
