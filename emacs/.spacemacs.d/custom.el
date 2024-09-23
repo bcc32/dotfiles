a91aecb15aa6a440d180cb15770d523e6e428108
Aaron L. Zeng <me@bcc32.com>
2024-09-23 01:42:33 -0400

    Display recipient when reading mail in /Sent folder

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -109,6 +109,12 @@
  '(mm-discouraged-alternatives '("text/html" "text/richtext" "image/.*"))
  '(mu4e-drafts-folder "/Drafts")
  '(mu4e-get-mail-command "offlineimap")
+ '(mu4e-headers-fields
+   '((:human-date . 12)
+     (:flags . 6)
+     (:mailing-list . 10)
+     (:from-or-to . 22)
+     (:subject)))
  '(mu4e-maildir-shortcuts
    '((:maildir "/INBOX" :key 105)
      (:maildir "/TODO.Input into ledger" :key 108)))
