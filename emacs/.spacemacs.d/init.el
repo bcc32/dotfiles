27a83a737aa925889f014b005f154cb6e47e3930
Aaron L. Zeng <me@bcc32.com>
2024-11-30 17:54:25 -0500

    bcc32/kill-ring-save-refill-for-web: Remove useless REGION arg

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -86,17 +86,13 @@ found, passing ARGS."
                     (format "start /wait /b %s" url))
     (apply #'browse-url-default-browser url args)))
 
-(defun bcc32/kill-ring-save-refill-for-web (beg end &optional region)
-  "Copy the region to the kill ring, refilling the text for web use.
+(defun bcc32/kill-ring-save-refill-for-web (beg end)
+  "Copy the region BEG to END to the kill ring, refilling the text for web use.
 
 When pasting into web forms, newlines separate paragraphs, unlike
 in Emacs where paragraphs are delimited by empty lines.  This
-command fills the copied text with no newlines within paragraphs.
-
-Use the region between BEG and END.  When called from Lisp and
-REGION is non-nil, use the current region instead of BEG and
-END."
-  (interactive (list (mark) (point) 'region))
+command fills the copied text with no newlines within paragraphs."
+  (interactive "r")
   (let ((filter-buffer-substring-function
          (lambda (beg end delete)
            (let ((contents (if delete
@@ -108,7 +104,7 @@ END."
                (indent-region (point-min) (point-max))
                (fill-region (point-min) (point-max))
                (buffer-string))))))
-    (kill-ring-save beg end region)))
+    (kill-ring-save beg end)))
 
 (defun bcc32/projectile-ignored-project-function (project-root)
   "Return t if a project rooted at PROJECT-ROOT should be ignored by projectile."
