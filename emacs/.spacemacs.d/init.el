c0b1b7cede1796e0cc2f899f7b0c9ce6da60f53e
Aaron L. Zeng <me@bcc32.com>
2025-01-24 10:35:58 -0500

    Disable line number display by default

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -657,8 +657,7 @@ It should only modify the values of Spacemacs settings."
    ;;   :size-limit-kb 1000)
    ;; When used in a plist, `visual' takes precedence over `relative'.
    ;; (default nil)
-   dotspacemacs-line-numbers
-   '(:visual t :disabled-for-modes org-mode)
+   dotspacemacs-line-numbers '(:visual t :enabled-for-modes)
 
    ;; Code folding method. Possible values are `evil', `origami' and `vimish'.
    ;; (default 'evil)
