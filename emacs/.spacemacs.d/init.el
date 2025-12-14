10334c1b672059670d90bc03efa5158c888d7ae8
Aaron L. Zeng <me@bcc32.com>
2025-12-14 02:06:45 -0500

    Remove unnecessary source-directory setting
    I added this in b1f9b5102adc57c85ee2e2a4be6bc559ec6f1841 for the sake
    of find-function-C-source-directory, but that is not necessary with
    the nixpkgs-packaged Emacs.

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -781,7 +781,7 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
-  (setq source-directory "~/src/emacs/trunk/"))
+  )
 
 (defun dotspacemacs/user-config ()
   "Configuration for user code:
