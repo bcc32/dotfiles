52bec63f4c1e6b158d4172f858e683787ba2e241
Aaron L. Zeng <azeng@janestreet.com>
2026-01-12 12:07:10 +0000

    shfmt stow.sh

--- stow.sh
+++ stow.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if command -v stow >/dev/null 2>&1; then
-    exec stow "$@"
+  exec stow "$@"
 else
-    exec perl -I ./.vendor/lib -- ./.vendor/bin/stow "$@"
+  exec perl -I ./.vendor/lib -- ./.vendor/bin/stow "$@"
 fi
