9475145473ba807bf4a60512df9b0a5b32531f3d
Aaron L. Zeng <me@bcc32.com>
2026-01-12 23:04:35 +0000

    Support --dotfiles option and move .ssh config

--- chmod.sh
+++ chmod.sh
@@ -4,4 +4,4 @@ set -eu
 
 cd "$(git rev-parse --show-toplevel)"
 
-chmod go-w ssh/.ssh/config
+chmod go-w ssh/dot-ssh/config
--- restow-all.sh
+++ restow-all.sh
@@ -25,4 +25,4 @@ for package in "${packages[@]}"; do
   fi
 done
 
-./stow.sh --restow --target "$HOME" "$@" "${filtered_packages[@]}"
+./stow.sh --restow --dotfiles --target "$HOME" "$@" "${filtered_packages[@]}"
