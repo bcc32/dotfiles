b20d3c6c78d6abc69aa2cb0c8cb7c86a4d3d1d66
Aaron L. Zeng <me@bcc32.com>
2026-02-03 13:38:30 -0500

    Remove magit-todos-exclude-globs
    Now that I'm not passing --hidden anymore, .vendor should be excluded
    by default.

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((magit-mode . ((magit-todos-exclude-globs . (".git/" ".vendor/")))))
