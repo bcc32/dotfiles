b45a9c3aab20226ac32acd2154c18432193208de
Aaron L. Zeng <me@bcc32.com>
2026-02-03 13:37:16 -0500

    Remove magit-todos-rg-extra-args argument
    Now that I'm using the --dotfiles argument for stow, my actual files
    do not contain the "." prefix anymore.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-mode . ((magit-todos-exclude-globs . (".git/" ".vendor/"))
-                (magit-todos-rg-extra-args . ("--hidden")))))
+((magit-mode . ((magit-todos-exclude-globs . (".git/" ".vendor/")))))
