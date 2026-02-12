515d73294e831461652def81b9459829889e94bd
Aaron L. Zeng <me@bcc32.com>
2026-02-11 20:37:22 -0500

    Switch back to storing offlineimap passphrase in plaintext on disk
    Ugh, it's too annoying to get password prompts failing in the
    background due to mu4e-update-interval.

--- offlineimap/dot-offlineimap.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from subprocess import check_output
-
-def get_pass():
-    return check_output("gpg -dq ~/src/dotfiles-private/offlineimap/password.gpg", shell=True).rstrip(b"\n")
