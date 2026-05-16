3a1cbec9a95e861ed88e894df4da036523ecfc24
Aaron L. Zeng <me@bcc32.com>
2026-05-16 19:08:04 -0400

    Disable dotspacemacs-smooth-scrolling

--- emacs/dot-spacemacs.d/init.el
+++ emacs/dot-spacemacs.d/init.el
@@ -602,7 +602,7 @@ It should only modify the values of Spacemacs settings."
    ;; If non-nil smooth scrolling (native-scrolling) is enabled. Smooth
    ;; scrolling overrides the default behavior of Emacs which recenters point
    ;; when it reaches the top or bottom of the screen. (default t)
-   dotspacemacs-smooth-scrolling t
+   dotspacemacs-smooth-scrolling nil
 
    ;; Show the scroll bar while scrolling. The auto hide time can be configured
    ;; by setting this variable to a number. (default t)
