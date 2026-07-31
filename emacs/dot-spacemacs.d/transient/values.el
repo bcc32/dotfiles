24ed3eb07fc6c8737ca23a9c6bceee2e6a12bf47
Aaron L. Zeng <me@bcc32.com>
2026-07-31 16:35:38 -0400

    Add -x to magit-cherry-pick by default
    This adds a comment pointing to the original commit.

--- emacs/dot-spacemacs.d/transient/values.el
+++ emacs/dot-spacemacs.d/transient/values.el
@@ -1,3 +1,4 @@
-((magit-diff:magit-stash-mode "--no-ext-diff")
+((magit-cherry-pick "-x")
+ (magit-diff:magit-stash-mode "--no-ext-diff")
  (magit-log:magit-status-mode "-n256" "--graph" "--decorate")
  (magit-submodule "--recursive"))
